afeae6779ef52cf1b13fdcbfc30f88c5a40fa783
SheikhSalmanBinTalib <59583800+SheikhSalmanBinTalib@users.noreply.github.com>
2024-06-01 17:52:31 +0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,7 @@
+let fruitchat = ["Banana", "Apple", "Orange"];
+//.push()
+fruitchat.pop();
+console.log(fruitchat);
+fruitchat = fruitchat.concat(["Melon", "Kiwi"]);
+console.log(fruitchat);
+export {};
